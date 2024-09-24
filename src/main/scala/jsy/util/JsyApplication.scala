c13806f270d9b1768503216186e04b7bd6ef9a0d
Bor-Yuh Evan Chang <bec@cs.colorado.edu>
2024-09-23 23:30:11 -0600

    Update JsyApplication with options in the same file

--- src/main/scala/jsy/util/JsyApplication.scala
+++ src/main/scala/jsy/util/JsyApplication.scala
@@ -4,11 +4,12 @@ import java.io.{ByteArrayOutputStream, File}
 import java.nio.file.Files
 
 trait JsyApplication {
-  import jsy.util.options
-  
   var debug = false /* set to false to disable debugging output */
   var keepGoing = true /* set to true to keep going after exceptions */
   var maxSteps: Option[Int] = None /* set to a number to bound the number of execution steps */
+
+  /*** Implement this method to process a file. ***/
+  def processFile(file: File): Unit
   
   var anonOption = ("input",
     { case filename :: Nil => new File(filename) }: PartialFunction[List[String], File],
@@ -31,8 +32,6 @@ trait JsyApplication {
     else try e catch {
       case exn: Throwable => println(exn.toString); default
     }
-    
-  def processFile(file: File): Unit
 
   def testJsy(file: File)(k: (File, File, => (Boolean, String)) => Unit): Unit = {
     val jsyext = """[.]jsy$""".r
@@ -89,5 +88,104 @@ trait JsyApplication {
     val file: File = opts.process(args)
     doFile(processFile, file)
   }
+}
 
+object options {
+  sealed abstract class Spec
+  case class SetBool(setter: Boolean => Unit, default: Option[Boolean => Boolean]) extends Spec
+  case class SetInt(setter: Option[Int] => Unit, default: Option[Boolean => Option[String]]) extends Spec
+
+  class Options[T](program: String, specs: List[(String, Spec, String)], anon: (String, PartialFunction[List[String], T], String)) {
+    val opts: Map[String, List[String] => Option[List[String]]] =
+      specs.foldLeft[Map[String, List[String] => Option[List[String]]]](Map.empty)((acc, spec) => spec match {
+        case (name, SetBool(setter, _), _) =>
+          acc +
+          (("--" + name) -> { (t: List[String]) => setter(true); Some(t) }) +
+          (("--no-" + name) -> { (t: List[String]) => setter(false); Some(t) })
+        case (name, SetInt(setter, _), _) =>
+          acc +
+          (("--" + name) -> { (t: List[String]) => t match {
+            case arg :: t =>
+              val argp: Int = try arg.toInt catch { case _: Throwable => usageErr() }
+              setter(Some(argp)); Some(t)
+            case _ => usageErr()
+          } }) +
+          (("--no-" + name) -> { (t: List[String]) => setter(None); Some(t) })
+      })
+      
+    val (anonName, anonDo, anonDesc) = anon
+      
+    val nameWidth: Int = opts.foldLeft(anonName.length){ case (acc, (n, _)) => acc max (n.length + 5) }
+    
+    def padRight(s: String, w: Int): String = {
+      val nspaces = (w - s.length) max 0
+      s + (" " * nspaces)
+    }
+    
+    def optline(name: String, text: String): String = {
+      "%-2s".format("") + padRight(name, nameWidth) + "  %s%n".format(text)
+    }
+      
+    val descriptions: String = {
+      (specs foldRight "")((spec, acc) => spec match {
+        case (name, SetBool(_, default), desc) => {
+          def defaultStr(b: Boolean): String =
+            default map (f => if (f(b)) " (default)" else "") getOrElse("") 
+          optline("--" + name, "turn on %s".format(desc) + defaultStr(true)) +
+          optline("--no-" + name, "turn off %s".format(desc) + defaultStr(false)) +
+          acc
+        }
+        case (name, SetInt(_, default), desc) => {
+          def defaultStr(b: Boolean): String = {
+            val opt =
+              for {
+                f <- default
+                s <- f(b)
+              } yield " (default%s)".format(s)
+            opt.getOrElse("")
+          }
+          optline("--" + name + " <int>", "set %s".format(desc) + defaultStr(true)) +
+          optline("--no-" + name, "unset %s".format(desc) + defaultStr(false)) +
+          acc
+        }
+      })
+    }
+    
+    val header =
+      """
+Usage: %s [options] %s
+        
+""".format(program, anonName) +
+optline(anonName, anonDesc) + """
+Options:
+"""
+
+    private var currentArgs: List[String] = List()
+
+    def usageErr(): Nothing = {
+      val unprocessed = currentArgs match {
+        case Nil => ""
+        case _ => """
+Unprocessed Arguments:
+  """ + currentArgs.reduceRight({ _ + " " + _ }) + "%n".format()
+      }
+      print(header + descriptions + unprocessed)
+      sys.exit(1)
+    }
+    
+    def process(args: Array[String]): T = {
+      def loop(l: List[String]): List[String] = {
+        currentArgs = l
+        l match {
+          case Nil => Nil
+          case h :: t => opts.get(h) match {
+            case None => l
+            case Some(doit) => { val tp = doit(t).getOrElse(t); loop(tp) }
+          }
+        }
+      }
+      val err: PartialFunction[List[String], T] = { case _ => usageErr() }
+      (anonDo orElse err)(loop(args.toList))
+    }
+  }
 }

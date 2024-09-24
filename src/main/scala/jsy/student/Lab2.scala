8e4a4c272b6d3dab025eaaf84fde36f39b9321d3
Bor-Yuh Evan Chang <bec@cs.colorado.edu>
2024-09-23 21:52:45 -0600

    Clean up Lab2.scala

--- src/main/scala/jsy/student/Lab2.scala
+++ src/main/scala/jsy/student/Lab2.scala
@@ -1,11 +1,7 @@
 package jsy.student
 
-import jsy.lab2.Lab2Like
-
-object Lab2 extends jsy.util.JsyApplication with Lab2Like {
-  import jsy.lab2.Parser
+object Lab2 {
   import jsy.lab2.ast._
-
   /*
    * CSCI 3155: Lab 2
    * <Your Name>
@@ -17,41 +13,23 @@ object Lab2 extends jsy.util.JsyApplication with Lab2Like {
   /*
    * Fill in the appropriate portions above by replacing things delimited
    * by '<'... '>'.
-   * 
-   * Replace the '???' expression with  your code in each function.
-   * 
-   * Do not make other modifications to this template, such as
-   * - adding "extends App" or "extends Application" to your Lab object,
-   * - adding a "main" method, and
-   * - leaving any failing asserts.
-   * 
-   * Your lab will not be graded if it does not compile.
-   * 
-   * This template compiles without error. Before you submit comment out any
-   * code that does not compile or causes a failing assert. Simply put in a
-   * '???' as needed to get something  that compiles without error. The '???'
-   * is a Scala expression that throws the exception scala.NotImplementedError.
    *
+   * Replace the '???' expression with your code in each function. The
+   * '???' expression is a Scala expression that throws a NotImplementedError
+   * exception.
+   *
+   * Your lab will not be graded if it does not compile.
+   *
+   * This template compiles without error. Before you submit comment out any
+   * code that does not compile or causes a failing assert.  Simply put in a
+   * '???' as needed to get something that compiles without error.
    */
-
-  /* We represent a variable environment as a map from a string of the
-   * variable name to the value to which it is bound.
-   * 
-   * You may use the following provided helper functions to manipulate
-   * environments, which are just thin wrappers around the Map type
-   * in the Scala standard library.  You can use the Scala standard
-   * library directly, but these are the only interfaces that you
-   * need.
-   */
-
-
 
   /* Some useful Scala methods for working with Scala values include:
    * - Double.NaN
    * - s.toDouble (for s: String)
    * - n.isNaN (for n: Double)
    * - n.isWhole (for n: Double)
-   * - s (for n: Double)
    * - s format n (for s: String [a format string like for printf], n: Double)
    *
    * You can catch an exception in Scala using:
@@ -83,6 +61,23 @@ object Lab2 extends jsy.util.JsyApplication with Lab2Like {
     }
   }
 
+  /* We represent a variable environment as a map from a string of the
+   * variable name to the value to which it is bound.
+   * 
+   * You may use the following provided helper functions to manipulate
+   * environments, which are just thin wrappers around the Map type
+   * in the Scala standard library.  You can use the Scala standard
+   * library directly, but these are the only interfaces that you
+   * need.
+   */
+  type Env = Map[String, Expr]
+  val empty: Env = Map()
+  def lookup(env: Env, x: String): Expr = env(x)
+  def extend(env: Env, x: String, v: Expr): Env = {
+    require(isValue(v))
+    env + (x -> v)
+  }
+
   def eval(env: Env, e: Expr): Expr = {
     e match {
       /* Base Cases */
@@ -92,26 +87,6 @@ object Lab2 extends jsy.util.JsyApplication with Lab2Like {
 
       case _ => ???
     }
-  }
-
-
-
-  /* Interface to run your interpreter from the command-line.  You can ignore what's below. */
-  def processFile(file: java.io.File): Unit =  {
-    if (debug) { println("Parsing ...") }
-
-    val expr = Parser.parseFile(file)
-
-    if (debug) {
-      println("\nExpression AST:\n  " + expr)
-      println("------------------------------------------------------------")
-    }
-
-    if (debug) { println("Evaluating ...") }
-
-    val v = eval(expr)
-
-     println(pretty(v))
   }
 
 }
